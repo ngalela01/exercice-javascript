dd967179a2088a219f0c297bf05ec9c85b38e3b9
NayirGalela <galela.nayir@gmail.com>
2024-01-27 01:24:56 +0100

    fin de l'exercice 9

--- JS-FORM-1/index.js
+++ JS-FORM-1/index.js
@@ -1,4 +1,21 @@
-let nom = document.getElementById("nom").value;
-alert(nom);
-let prenom = document.getElementById("prenom").value;
-alert(prenom);
+// let nom = document.getElementById("nom").value;
+// alert(nom);
+// let prenom = document.getElementById("prenom").value;
+// alert(prenom);
+
+//exercice9 calculatrice
+
+let premierNombre = document.getElementById("premier-nombre").value;
+let deuxiemeNombre = document.getElementById("deuxieme-nombre").value;
+function addition() {
+  let somme = parseInt(premierNombre) + parseInt(deuxiemeNombre);
+
+  return somme;
+}
+
+let boutton = document.getElementById("mon-boutton");
+
+boutton.addEventListener("click", () => {
+  document.getElementById("resultat").textContent =
+    "le resulat est de " + addition();
+});
